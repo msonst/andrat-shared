710ee7b694125e400c978ca4f8df5921e8a7d1c4
sonst00m <spamdorado@googlemail.com>
2025-02-27 17:02:22 +0100

    transfer

--- src/main/java/com/cs/pc/FileStorage.java
+++ src/main/java/com/cs/pc/FileStorage.java
@@ -1,7 +1,9 @@
 package com.cs.pc;
 
 import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -19,7 +21,7 @@ public class FileStorage {
 		this.mBaseDir = mBaseDir;
 	}
 
-	public String store(TransferObject data) throws IOException {
+	public synchronized String store(TransferObject data) throws IOException {
 		String fileName = System.currentTimeMillis() + "-" + UUID.randomUUID().toString() + ".dat";
 
 		String type = data.getType().toString();
@@ -42,6 +44,43 @@ public class FileStorage {
 		Files.move(tmpPath, readyPath, StandardCopyOption.REPLACE_EXISTING);
 
 		return fileName;
+	}
+
+	public synchronized TransferObject load(String fileName) {
+		// Deriving the path to the ready directory
+		Path readyPath = Path.of(mBaseDir, READYDIR, fileName);
+
+		if (Files.exists(readyPath)) {
+			// Open the file and deserialize the object
+			try (FileInputStream fileInputStream = new FileInputStream(readyPath.toFile());
+					ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
+
+				// Read the object and cast to TransferObject
+				return (TransferObject) objectInputStream.readObject();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
+	public synchronized boolean delete(String fileName) {
+		Path readyPath = Path.of(mBaseDir, READYDIR, fileName);
+
+		if (Files.exists(readyPath)) {
+			try {
+				Files.delete(readyPath); // Delete the file from the ready directory
+				return true; // Return true if file is successfully deleted
+			} catch (IOException e) {
+				// Log and return false in case of an error
+				e.printStackTrace();
+				return false;
+			}
+		} else {
+			// Log and return false if the file doesn't exist
+			System.err.println("File not found: " + readyPath.toString());
+			return false;
+		}
 	}
 
 	public static byte[] toByteArray(Object object) throws IOException {
--- src/main/java/com/cs/pc/TransferObject.java
+++ src/main/java/com/cs/pc/TransferObject.java
@@ -1,25 +1,40 @@
 package com.cs.pc;
 
-public class TransferObject {
+import java.io.Serializable;
 
-	public enum Type{
-		GPS, SMS, CALL, BT_DEV, GEOFENCE, AUDIO, WHATSAPP, LOG,  USER_INPUT, CONTACT;
+public class TransferObject implements Serializable {
+
+	public enum Type {
+		GPS, SMS, CALL, BT_DEV, GEOFENCE, AUDIO, WHATSAPP, LOG, USER_INPUT, CONTACT, EVT;
 	}
-	
+
 	private Type mType;
 	private byte[] mData;
-	
+
+	public TransferObject() {
+
+	}
+
+	public TransferObject(Type type, byte[] data) {
+		super();
+		mType = type;
+		mData = data;
+	}
+
 	public Type getType() {
 		return mType;
 	}
+
 	public void setType(Type type) {
 		mType = type;
 	}
+
 	public byte[] getData() {
 		return mData;
 	}
+
 	public void setData(byte[] data) {
 		mData = data;
 	}
-	
+
 }
