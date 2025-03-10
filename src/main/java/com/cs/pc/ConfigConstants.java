f6bf433e95752cbc0842325ad489d3f5b8f45699
sonst00m <spamdorado@googlemail.com>
2025-03-10 18:34:00 +0100

    cfg, types

---
+++ src/main/java/com/cs/pc/ConfigConstants.java
@@ -0,0 +1,29 @@
+package com.cs.pc;
+
+public class ConfigConstants {
+	public static final String GPS_UPLOAD_EN = "GPS_UPLOAD_EN";
+	public static final String GPS_UPLOAD_INTERVAL = "GPS_UPLOAD_INTERVAL";
+
+	public static final String SMS_UPLOAD_EN = "SMS_UPLOAD_EN";
+	public static final String SMS_UPLOAD_INTERVAL = "SMS_UPLOAD_INTERVAL";
+
+	public static final String CALL_UPLOAD_EN = "CALL_UPLOAD_EN";
+	public static final String CALL_UPLOAD_INTERVAL = "CALL_UPLOAD_INTERVAL";
+
+	public static final String WHATSAPP_UPLOAD_EN = "WHATSAPP_UPLOAD_EN";
+	public static final String WHATSAPP_UPLOAD_INTERVAL = "WHATSAPP_UPLOAD_INTERVAL";
+
+	public static final String FILE_UPLOAD_EN = "FILE_UPLOAD_EN";
+	public static final String FILE_UPLOAD_INTERVAL = "FILE_UPLOAD_INTERVAL";
+
+	public static final String AUDIO_UPLOAD_EN = "AUDIO_UPLOAD_EN";
+	public static final String AUDIO_UPLOAD_INTERVAL = "AUDIO_UPLOAD_INTERVAL";
+
+	public static final String AUDIO_STREAM_EN = "AUDIO_STREAM_EN";
+
+	public static final String JSON_UPLOAD_BATCH_SIZE = "JSON_UPLOAD_BATCH_SIZE";
+	public static final String FILE_UPLOAD_BATCH_SIZE = "FILE_UPLOAD_BATCH_SIZE";
+
+	public static final String CONFIG_CHECK_EN = "CONFIG_CHECK_EN";
+	public static final String CONFIG_CHECK_INTERVAL = "CONFIG_CHECK_INTERVAL";
+}
